dd2fe5fab9786fd81c25553e8d6dccce3247c23f
Malakai <poncemalakai@gmail.com>
2025-11-05 20:55:15 -0800

    mapping everything

--- lab_29.cpp
+++ lab_29.cpp
@@ -4,4 +4,17 @@
 // Lab 29: Coffee Shop Simulation
 //------------------------------------------------------------------------------
 
+// Order:
+    // id: string
+    // customer : string
+    // drink : string
+    // size : string
+    // makeTime : int
+
+// Phase lists = arary of 3 lsits
+    // [0] queue
+    // [1] inProgress
+    // [2] completed
+
+//baristas: map<string, Stagelist>> // key = baristas name
 
